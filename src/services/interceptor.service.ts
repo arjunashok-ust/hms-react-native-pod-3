6ddc76b699f63bdad842dceea2021f5af697a009
Arjun <Arjun.Ashok@ust.com>
2026-06-24 17:47:25 +0530

    added: access and refresh token handling

--- src/services/interceptor.service.ts
+++ src/services/interceptor.service.ts
@@ -2,25 +2,61 @@ import axios from "axios";
 import * as SecureStore from "expo-secure-store";
 import { Alert } from "react-native";
 import Constants from "expo-constants";
+import { jwtDecode } from "jwt-decode";
 
 const API_URL = Constants.expoConfig?.extra?.API_URL;
 
 const api = axios.create({
   baseURL: API_URL,
+  withCredentials: true,
 });
+
+let isRefreshing: boolean = false;
+let newTokenPromise: Promise<string> | null = null;
 
 // token attacher
 api.interceptors.request.use(
   async (config) => {
     // skipping token for login and signup
-    if (config.url === "auth/login" || config.url === "auth/patientSignUp") {
+    if (
+      config.url === "auth/login" ||
+      config.url === "auth/patientSignUp" ||
+      config.url === "auth/refresh-token"
+    ) {
       return config;
     }
 
     const token = await SecureStore.getItemAsync("token");
-
     if (!token) {
       throw new Error("No Authentication Found");
+    }
+
+    const decoded = jwtDecode<{ exp: number }>(token ?? "");
+    const isExpired = decoded.exp * 1000 < Date.now();
+
+    if (isExpired) {
+      if (!isRefreshing) {
+        isRefreshing = true;
+        newTokenPromise = api
+          .get("auth/refresh-token")
+          .then(async (res) => {
+            const newToken = res.data.token;
+            await SecureStore.setItemAsync("token", newToken);
+            return newToken;
+          })
+          .catch(async (error) => {
+            await SecureStore.deleteItemAsync("token");
+            Alert.alert("Session Expired", "Please login again.");
+            throw error;
+          })
+          .finally(() => {
+            isRefreshing = false;
+            newTokenPromise = null;
+          });
+      }
+      const newToken = await newTokenPromise!;
+      config.headers.Authorization = `Bearer ${newToken}`;
+      return config;
     }
 
     config.headers.Authorization = `Bearer ${token}`;
@@ -34,11 +70,11 @@ api.interceptors.request.use(
 // centralized error handling system
 api.interceptors.response.use(
   (response) => response,
-  (error) => {
+  async (error) => {
     let message = "";
 
     if (error.response) {
-      message = 
+      message =
         error.response?.data?.message || `Error ${error.response.status}`;
     } else if (error.request) {
       message = "No response from the server";
@@ -48,7 +84,7 @@ api.interceptors.response.use(
 
     Alert.alert("Failed", message);
 
-    return Promise.reject(error);
+    throw error;
   },
 );
 
